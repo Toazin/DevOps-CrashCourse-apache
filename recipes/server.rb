df4d22747c676cae4374adc1efab6fb3af571cc5
ChefDK User <chef@ip-10-0-0-186.ec2.internal>
2017-06-17 22:12:31 +0000

    Template added with generator

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,12 +1,17 @@
 package	'httpd'	do
         action :install
 end
-file '/var/www/html/index.html'	do
-        content	"<h1> Hello, world_ </h1>
-		<h3><b>IP Address:</b> #{node['ipaddress']}  </h3>
-		<h3><b>Hostname:</b> #{node['hostname']}  </h3>"
-	
+
+#file '/var/www/html/index.html'        do
+#        content        "<h1> Hello, world_ </h1>
+#               <h3><b>IP Address:</b> #{node['ipaddress']}  </h3>
+#               <h3><b>Hostname:</b> #{node['hostname']}  </h3>"
+
+
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
+	
 service	'httpd'	do
         action [:enable , :start]
 end
